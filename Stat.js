ca779aea76a5a8acbb18aa55fad8e945e7a1d76c
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-01-20 22:38:38 -0800

    Define Stat.sum method

--- Stat.js
+++ Stat.js
@@ -3,6 +3,12 @@ var Stat = {
     if (!Array.isArray(array)) {
       throw new Error('This method requires a numeric array as input');
     }
+  },
+  sum: function(array) {
+    Stat.requireArray(array);
+    return array.reduce(function(prev, next) {
+      return (prev + next);
+    });
   }
 };
 
