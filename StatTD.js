a6c64bbf95ae5f6af1c104889a08ec0a2c5851ea
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-01-20 22:34:39 -0800

    Define 'test' function to test expected behavior from Stat.js

--- StatTD.js
+++ StatTD.js
@@ -1,1 +1,12 @@
 var Stat = require('./Stat.js');
+
+var test = function(method, testArray, expected) {
+  var message;
+  var computed = method(testArr);
+  if (computed === expected) {
+    message = 'success';
+  } else {
+    message = 'failure: expected ' + computed + ' to be ' + expected;
+  }
+  console.log(message);
+};
