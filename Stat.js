b260cbc60f63184fbaa2d6a7d64c22b792d96df9
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-01-20 22:40:47 -0800

    Define Stat.mean method

--- Stat.js
+++ Stat.js
@@ -9,6 +9,10 @@ var Stat = {
     return array.reduce(function(prev, next) {
       return (prev + next);
     });
+  },
+  mean: function(array) {
+    Stat.requireArray(array);
+    return (Stat.sum(array) / array.length);
   }
 };
 
