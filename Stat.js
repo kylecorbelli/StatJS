3890e294e9d065af2d8820348bd083bace929b1a
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-01-20 22:37:43 -0800

    Define Stat.requireArray method

--- Stat.js
+++ Stat.js
@@ -1,3 +1,9 @@
-var Stat = {};
+var Stat = {
+  requireArray: function(array) {
+    if (!Array.isArray(array)) {
+      throw new Error('This method requires a numeric array as input');
+    }
+  }
+};
 
 module.exports = Stat;
