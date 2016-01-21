682f77d7f59e9743bb997461ce292d42065e509e
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-01-21 07:04:41 -0800

    Write test for existence of methods. Add beforeEach statement

--- test/StatSpec.js
+++ test/StatSpec.js
@@ -6,6 +6,20 @@
 
   describe('StatJS', function() {
 
+    var testArr;
+
+    beforeEach(function() {
+      testArr = [7, 4, 4.3, -1, 3.14, 8, 2, 9.22221, 4];
+    });
+
+    it('should have methods "requiredArray", "sum", "mean", "variance", "stdev"', function() {
+      expect(Stat.requireArray).to.be.a('function');
+      expect(Stat.sum).to.be.a('function');
+      expect(Stat.mean).to.be.a('function');
+      expect(Stat.variance).to.be.a('function');
+      expect(Stat.stdev).to.be.a('function');
+    });
+
   });
 
 })();
